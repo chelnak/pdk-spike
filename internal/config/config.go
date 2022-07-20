754eb2d2b8f7f77acca731e30b1abbae244809b6
Craig Gumbley <craiggumbley@gmail.com>
2022-07-20 13:20:39 +0100

    Add print commands
    This commit enables the caller to print the current viper config to the
    terminal in either json or yaml format.

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,20 @@
+// Package config is responsible for managing the configuration of the pdk.
 package config
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 
+	"github.com/alecthomas/chroma"
+	"github.com/alecthomas/chroma/formatters"
+	"github.com/alecthomas/chroma/lexers"
+	"github.com/alecthomas/chroma/styles"
+
 	"github.com/spf13/viper"
+	"gopkg.in/yaml.v2"
 )
 
 func InitConfig(cfgFile string) error {
@@ -57,4 +66,88 @@ func setDefaults() {
 	viper.SetDefault("tool_args", "")
 	viper.SetDefault("tool_path", "") // this should be configured config directory /tools
 	viper.SetDefault("tool_timeout", 1800)
+}
+
+type writeOptions struct {
+	data      string
+	lexerName string
+	noColor   bool
+	writer    io.Writer
+}
+
+// Should change to use Puppet colors
+func prettyWrite(opts writeOptions) error {
+	lexer := lexers.Get(opts.lexerName)
+	if lexer == nil {
+		lexer = lexers.Fallback
+	}
+
+	lexer = chroma.Coalesce(lexer)
+
+	style := styles.Get("native")
+	if style == nil {
+		style = styles.Fallback
+	}
+
+	formatter := formatters.Get("terminal16m")
+
+	if opts.noColor {
+		formatter = formatters.Get("noop")
+	}
+
+	iterator, err := lexer.Tokenise(nil, opts.data)
+	if err != nil {
+		return err
+	}
+
+	return formatter.Format(opts.writer, style, iterator)
+}
+
+// PrintJSON prints the current configuration to the terminal in JSON format.
+func PrintJSON(noColor bool, writer io.Writer) error {
+	var ifac interface{}
+	err := viper.Unmarshal(&ifac)
+	if err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(ifac, "", "  ")
+	b = append(b, '\n')
+	if err != nil {
+		return err
+	}
+
+	opts := writeOptions{
+		data:      string(b),
+		lexerName: "json",
+		noColor:   noColor,
+		writer:    writer,
+	}
+
+	return prettyWrite(opts)
+}
+
+// PrintYAML prints the current configuration to the terminal in YAML format.
+func PrintYAML(noColor bool, writer io.Writer) error {
+	var ifac interface{}
+	err := viper.Unmarshal(&ifac)
+	if err != nil {
+		return err
+	}
+
+	b, err := yaml.Marshal(ifac)
+	y := []byte("---\n")
+	y = append(y, b...)
+	if err != nil {
+		return err
+	}
+
+	opts := writeOptions{
+		data:      string(y),
+		lexerName: "yaml",
+		noColor:   noColor,
+		writer:    writer,
+	}
+
+	return prettyWrite(opts)
 }
